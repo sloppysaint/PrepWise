f2ab9e24e74d79808bf203d239d67aa5cf4e45f1
Piyush2604 <khattrapiyush80@gmail.com>
2025-05-27 01:40:59 +0530

    doing auth

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,30 +1,82 @@
 "use client"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import {zodResolver} from "@hookform/resolvers/zod"
+import {useForm} from "react-hook-form"
+import {z} from "zod"
+import {Button} from "@/components/ui/button"
+import {
+    Form,
+} from "@/components/ui/form"
+import Image from "next/image";
+import Link from "next/link";
+import {toast} from "sonner";
 
-const formSchema = z.object({
-    username: z.string().min(2).max(50),
-})
+
+const authFormSchema = (type: FormType) => {
+    return z.object({
+        name: type === "sign-up" ? z.string().min(3) : z.string().optional(),
+        email: z.string().email(),
+        password: z.string().min(3),
+    })
+}
 
 
-const AuthForm = () => {
+const AuthForm = ({type}: { type: FormType }) => {
+
+    const formSchema = authFormSchema(type);
     // 1. Define your form.
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            username: "",
+            name: "",
+            email: "",
+            password: "",
+
         },
     })
 
     // 2. Define a submit handler.
     function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+        try {
+            if (type === "sign-up") {
+                console.log('SIGN UP', values)
+            } else {
+                console.log('SIGN IN', values)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(`There was an error: ${error}`)
+        }
     }
+
+    const isSignIn = type === "sign-in";
+
     return (
-        <div>AuthForm</div>
+        <div className="card-border lg:min-w-[566px]">
+            <div className="flex flex-col gap-6 card py-14 px-10">
+                <div className="flex flex-row gap-2 justify-center">
+                    <Image src="/logo.svg" alt="logo" width={38} height={32}/>
+
+                    <h2 className="text-primary-100">PrepWise</h2>
+                </div>
+                <h3>Practice job interviews with AI</h3>
+
+
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
+                        {!isSignIn && <p>Name</p>}
+                        <p>Email</p>
+                        <p>Password</p>
+                        <Button className="btn" type="submit">{isSignIn ? 'Sign in' : 'Create an Account'}</Button>
+                    </form>
+                </Form>
+
+                <p className="text-center">{isSignIn ? 'No account yet?' : 'Have an account already?'}
+                    <Link href={!isSignIn ? '/sign-in' : '/sign-up'}
+                          className="font-bold text-user-primary ml-1"> {!isSignIn ? "Sign in" : 'Sign up'} </Link>
+                </p>
+
+            </div>
+        </div>
     )
 }
 export default AuthForm
