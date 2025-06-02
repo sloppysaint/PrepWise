fa72f912e9d7e04ca398fb2bff076cbb266d27c6
Piyush2604 <khattrapiyush80@gmail.com>
2025-06-03 03:18:53 +0530

    building home-page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
 
 const Page = () => {
     return (
-        <div>Home Page</div>
+        <>
+            <section className="card-cta">
+                <div className="flex flex-col gap-6 max-w-lg">
+                    <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+                    <p className="text-lg">Practice on Real interview questions & get instant feedback</p>
+                    <Button asChild className="btn-primary max-sm:w-full">
+                        <Link href="/interview">Start an Interview</Link>
+                    </Button>
+                </div>
+
+                <Image src="/robot.png" alt="illustration" width={400} height={400} className="max-sm:hidden"/>
+            </section>
+
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Your Interviews</h2>
+                <div className="interviews-section">
+                    <p>You haven&apos;t taken any interviews yet</p>
+                </div>
+
+            </section>
+        </>
     )
 }
 export default Page
