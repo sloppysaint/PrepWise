0331581ff3493c58f01a805f2ffa42e375177825
Piyush2604 <khattrapiyush80@gmail.com>
2025-06-05 16:36:48 +0530

    implement interview generation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 import {isAuthenticated} from "@/lib/actions/auth.action";
 import {redirect} from "next/navigation";
 
-const RootLayout = async ({children}: {children: ReactNode}) => {
+const RootLayout =  async ({children}: {children: ReactNode}) => {
 
     const isUserAuthenticated = await isAuthenticated();
 
-    if (!isUserAuthenticated) redirect('/sign-in')
+    if (!isUserAuthenticated) redirect("/sign-in")
     return (
         <div className="root-layout">
             <nav>
